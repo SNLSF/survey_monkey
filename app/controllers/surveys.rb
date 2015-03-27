4b50f6cef1af23e0722c3c1a7e7059ff803ecc36
OaklandRocks & tonymai <pair+oaklandrocks.tonymai@devbootcamp.com>
2015-03-27 12:41:23 -0700

    midway through new survey creation

--- app/controllers/surveys.rb
+++ app/controllers/surveys.rb
@@ -4,11 +4,22 @@ end
 
 get '/surveys/new' do
   # goes to the form to create a new survey
+  erb :'surveys/new'
 end
 
-post '/surveys/:id' do
-  # writes the survey to the server and redirects to the survey's page
+post '/surveys/new' do
+  survey = Survey.create(title: params[:title])
+
+  new_questions = []
+  params[:question].each do |k,v|
+    new_questions << survey.questions.create(content: v)
+  end
+
+  new_questions.each_with_index()
+  # survey.questions.create(content: params[:co])
+  ""
 end
+
 
 get '/surveys/:id' do
   # shows the specific survey
--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,38 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+  var question_counter = 0;
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $(".add-question").on("click", function(event){
+    event.preventDefault();
+    var choice_counter = 0;
+    question_counter++;
+
+    $(".questions").append("Question "+question_counter+": <div data-question_id="+question_counter+"><input type='text' name='question["+question_counter+"]'> <button class='add-choice'>Add Choice</button></div><br />")
+  })
+
+  $('.questions').delegate('.add-choice','click',function(event) {
+    event.preventDefault();
+    var choice_counter = $(this).parent().children('input').length;
+    var question_id = $(this).parent().attr('data-question_id')
+    $(this).after("<input type='text' name='choice["+question_id+"]["+choice_counter+"]'>")
+    console.log(this);
+  })
 });
+
+// grab all the divs with class quesions
+
+
+// name="question[#{id}]"
+// name="question[1]"
+// name="question[2]"
+
+// $('.questions').children().length
+
+// params[:question] # { 1: "fewf", 2: "fewf"}
+
+name="choice[1][1]"
+name="choice[1][2]"
+name="choice[1][3]"
+
+name="choice[2][1]"
+name="choice[2][2]"
+name="choice[2][3]"
